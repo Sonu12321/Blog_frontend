b3395b059a5a99b60ca60fc16e6b240136b0ddb7
Sonu Santhosh <sonusanthosh371@gmail.com>
2024-10-04 18:26:35 +0530

    comments

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
-// import Comment from './Comment';
+
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState('');
@@ -12,8 +12,6 @@ export default function CommentSection({ postId }) {
   const [comments, setComments] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [commentToDelete, setCommentToDelete] = useState(null);
-  console.log(comments,"Sdsd");
-  
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -57,42 +55,42 @@ export default function CommentSection({ postId }) {
     };
     getComments();
   }, [postId]);
-//9:10
-  // const handleLike = async (commentId) => {
-  //   try {
-  //     if (!currentUser) {
-  //       navigate('/sign-in');
-  //       return;
-  //     }
-  //     const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-  //       method: 'PUT',
-  //     });
-  //     if (res.ok) {
-  //       const data = await res.json();
-  //       setComments(
-  //         comments.map((comment) =>
-  //           comment._id === commentId
-  //             ? {
-  //                 ...comment,
-  //                 likes: data.likes,
-  //                 numberOfLikes: data.likes.length,
-  //               }
-  //             : comment
-  //         )
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
 
-  // const handleEdit = async (comment, editedContent) => {
-  //   setComments(
-  //     comments.map((c) =>
-  //       c._id === comment._id ? { ...c, content: editedContent } : c
-  //     )
-  //   );
-  // };
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
 
   const handleDelete = async (commentId) => {
     setShowModal(false);
@@ -112,7 +110,7 @@ export default function CommentSection({ postId }) {
       console.log(error.message);
     }
   };
-   return (
+  return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
         <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
@@ -164,7 +162,7 @@ export default function CommentSection({ postId }) {
           )}
         </form>
       )}
-       {comments.length === 0 ? (
+      {comments.length === 0 ? (
         <p className='text-sm my-5'>No comments yet!</p>
       ) : (
         <>
@@ -178,17 +176,17 @@ export default function CommentSection({ postId }) {
             <Comment
               key={comment._id}
               comment={comment}
-              // onLike={handleLike}
-              // onEdit={handleEdit}
-              // onDelete={(commentId) => {
-              //   setShowModal(true);
-              //   setCommentToDelete(commentId);
-              // }}
+              onLike={handleLike}
+              onEdit={handleEdit}
+              onDelete={(commentId) => {
+                setShowModal(true);
+                setCommentToDelete(commentId);
+              }}
             />
           ))}
         </>
       )}
-      {/* <Modal
+      <Modal
         show={showModal}
         onClose={() => setShowModal(false)}
         popup
@@ -214,7 +212,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
         </Modal.Body>
-      </Modal> */ }
+      </Modal>
     </div>
   );
 }
